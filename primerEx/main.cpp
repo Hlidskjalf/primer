7c8c969e0357915a63158f98f61e570989faf32d
Andrew <user@users-macbook-air.local>
2017-01-01 23:09:46 -0500

    Initial Commit

---
+++ primerEx/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  primerEx
+//
+//  Created by Andrew Vroman on 1/1/17.
+//  Copyright © 2017 com.andrewvroman. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
