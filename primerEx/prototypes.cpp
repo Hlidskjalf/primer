7e358aa2a16589af99c8775ee9068a04f2964c19
Andrew <user@users-macbook-air.local>
2017-01-06 12:55:18 -0500

    section 1.4.4 (updated comments and formatting)

--- primerEx/prototypes.cpp
+++ primerEx/prototypes.cpp
@@ -23,17 +23,23 @@ int printTest()
 
 int counter()
 {
+    /** Declares 3 values, the current value being read, the value to be stored, and the count
+        per value. On each iteration we read the current value to ensure it exists, compare it
+        to the stored value, and if they are the same print the count. If they are different,
+        then we simply print the total of that value.
+     */
+    
     int currVal = 0, val = 0;
-    // read the first number and ensure that we have data to process
+                                   // read the first number and ensure that we have data to process
     if (std::cin >> currVal) {
-        int cnt = 1;  // store the count for the current value we are processing
+        int cnt = 1;               // store the count for the current value we are processing
         while (std::cin >> val) {
-            if (val == currVal)
-                ++cnt;
-            else {
+            if (val == currVal)    // if the value stored equals the value being read,
+                ++cnt;             // increment the counter.
+            else {                 // print the count of the values
                 std::cout << currVal << " occurrs " << cnt << " times." << std::endl;
-                currVal = val;  // to remember the new value
-                cnt = 1;  // reset the counter
+                currVal = val;     // to remember the new value
+                cnt = 1;           // reset the counter
             }
         }
         
